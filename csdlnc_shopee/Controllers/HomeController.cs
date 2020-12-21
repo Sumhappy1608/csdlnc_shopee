e12810443df923c5ca2f22d620a2472f4f3b1fe5
GaoXteen <thai.nguyenhong90@gmail.com>
2020-12-22 00:41:35 +0700

    search by category

--- csdlnc_shopee/Controllers/HomeController.cs
+++ csdlnc_shopee/Controllers/HomeController.cs
@@ -7,14 +7,25 @@ using csdlnc_shopee.Models;
 
 namespace csdlnc_shopee.Controllers
 {
-    
+
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             shopeeEntities db = new shopeeEntities();
             List<HANGHOA> pList = db.HANGHOAs.ToList();
             return View(pList);
+        }
+
+        public ActionResult ProductMan()
+        {
+            shopeeEntities db = new shopeeEntities();
+            List<HANGHOA> pList = (from HH in db.HANGHOAs
+                                   join DM in db.DANHMUCs on HH.LOAIHANGHOA equals DM.MALOAI
+                                   where DM.TENLOAI == "MAN"
+                                   select HH).ToList();
+            return PartialView("_ProductMan", pList);
         }
 
         //Lấy chi tiết sản phẩm
@@ -26,6 +37,18 @@ namespace csdlnc_shopee.Controllers
             return View(product);
         }
 
+        public ActionResult Categories()
+        {
+            shopeeEntities db = new shopeeEntities();
+            List<DANHMUC> cList = db.DANHMUCs.ToList();
+            return PartialView("_Categories", cList);
+        }
+        public ActionResult CategoryItem(string categoryID)
+        {
+            shopeeEntities db = new shopeeEntities();
+            List<HANGHOA> cList = db.HANGHOAs.Where(x => x.LOAIHANGHOA == categoryID).ToList();
+            return View(cList);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
